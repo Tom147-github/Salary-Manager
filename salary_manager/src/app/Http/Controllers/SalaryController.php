55de2ea07e626f04379e7968c6ddff626f8eb97b
konagaya <cresta522@gmail.com>
2023-02-04 21:01:16 +0900

    refactoring

--- salary_manager/src/app/Http/Controllers/SalaryController.php
+++ salary_manager/src/app/Http/Controllers/SalaryController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\DayAndSalaryAmount;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class SalaryController extends Controller
 {
@@ -15,24 +16,25 @@ class SalaryController extends Controller
     public function updateResult(Request $request)
     {
         //入力された日付のデータがDBに存在しない場合は新規作成し、存在する場合は上書き
-        DayAndSalaryAmount::updateOrCreate(
-            [ 'day' => $request->get("day") ],
-            [ 'amount' => $request->get("amount") ]
+        DayAndSalaryAmount::query()->updateOrCreate(
+            ['day' => $request->get("day")],
+            ['amount' => $request->get("amount")]
         );
 
         /*
          * ・DBに登録された１日から３１日までのそれぞれの日の金額を、日付をキーとして取得し、配列を生成
          * ・対象の日付のデータが存在しない場合はnullが格納される
          */
-        $day_amount_data = DayAndSalaryAmount::query()
-                                             ->select(['amount', 'day'])
-                                             ->orderBy('day')
-                                             ->get();
+        $day_amounts = DayAndSalaryAmount::query()
+            ->select(['amount', 'day'])
+            ->orderBy('day')
+            ->get();
         $amounts = [];
-        for ($day = 1; $day <= 31; $day++) {
-            $amounts[$day] = $day_amount_data->where('day', '=', $day)
-                                             ->pluck('amount')
-                                             ->first();
+        $today = Carbon::today();
+        for ($day = 1; $day <= $today->endOfMonth(); $day++) {
+            $amounts[$day] = $day_amounts->where('day', '=', $day)
+                ->pluck('amount')
+                ->first();
         }
 
         //Tableに表示されている全ての金額を合計し、変数に格納
@@ -41,24 +43,25 @@ class SalaryController extends Controller
             $total_amount += $amount;
         }
 
-        return view('result', compact('amounts', 'total_amount'));
+        $eom = Carbon::today();
+        return view('result', compact('amounts', 'total_amount', "eom"));
     }
 
     public function resetSalaryTable()
     {
         //DBリセット
-        $all_day_amount_data = DayAndSalaryAmount::all();
-        $all_day_amount_data->each->delete();
+        DayAndSalaryAmount::query()->delete();
 
         //Tableリセット
         $amounts = [];
-        for ($day = 1; $day <= 31; $day++) {
+        $today = Carbon::today();
+        for ($day = 1; $day <= $today->endOfMonth(); $day++) {
             $amounts[$day] = null;
         }
 
         //合計金額リセット
         $total_amount = 0;
-
-        return view('result', compact('amounts', 'total_amount'));
+        $eom = Carbon::today();
+        return view('result', compact('amounts', 'total_amount', "eom"));
     }
 }
--- salary_manager/src/resources/views/result.blade.php
+++ salary_manager/src/resources/views/result.blade.php
@@ -4,39 +4,37 @@
     <title>Salary Manager</title>
 </head>
 <body>
-    <form action="{{ route('update.result') }}" method="post">
-        @csrf
-        <label for="day">Day:</label><br>
-        <input type="number" name="day" id="day"><br>
-        <label for="amount">Amount:</label><br>
-        <input type="number" name="amount" id="amount"><br>
-        <button type="submit" value="Submit"
-        style="margin: 20px 0px 15px 0px;">Submit</button>
-    </form>
+<form action="{{ route('update.result') }}" method="post">
+    @csrf
+    <label for="day">Day:</label><br>
+    <input type="number" name="day" id="day"><br>
+    <label for="amount">Amount:</label><br>
+    <input type="number" name="amount" id="amount"><br>
+    <button type="submit" value="Submit" style="margin: 20px 0px 15px 0px;">Submit</button>
+</form>
 
-    <h2>Total Amount : {{ $total_amount }}</h2>
+<h2>Total Amount : {{ $total_amount }}</h2>
 
-    <table border="1">
-        <thead>
-            <tr>
-                <th>Day</th>
-                <th>Amount</th>
-            </tr>
-        </thead>
-        <tbody>
-            @for ($day = 1; $day <= 31; $day++)
-                <tr>
-                    <td>{{ $day }}</td>
-                    <td>{{ $amounts[$day] }}</td>
-                </tr>
-            @endfor
-        </tbody>
-    </table>
+<table border="1">
+    <thead>
+    <tr>
+        <th>Day</th>
+        <th>Amount</th>
+    </tr>
+    </thead>
+    <tbody>
+    @for ($day = 1; $day <= $eom; $day++)
+        <tr>
+            <td>{{ $day }}</td>
+            <td>{{ $amounts[$day] }}</td>
+        </tr>
+    @endfor
+    </tbody>
+</table>
 
-    <form action="{{ route('reset.salary.table') }}" method="post">
-        @csrf
-        <button type="submit" value="Reset"
-        style="margin: 60px 0px 0px 0px;">Reset</button>
-    </form>
+<form action="{{ route('reset.salary.table') }}" method="post">
+    @csrf
+    <button type="submit" value="Reset" style="margin: 60px 0px 0px 0px;">Reset</button>
+</form>
 </body>
 </html>
